f8588e0cb9cafdb6c756762fef72743c6ce3b707
obrovec913 <boroda9133@yandex.by>
2023-05-05 00:52:39 +0300

    add dicr

--- apars.py
+++ apars.py
@@ -8,7 +8,7 @@ he =  {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.3
 def savePars(fi: str, words:str, ):
     o = open(f'dictionary\\1{words}.txt', 'a+', encoding='utf-8')
     so = bs(fi, "lxml")
-    results = so.find( class_='para-concordance')
+    results = so.find( class_='kwic-list')
     print()
     # Extract the search results
     for result in results.find_all(class_='hit word'):
--- mai.py
+++ mai.py
@@ -10,8 +10,8 @@ from selenium.webdriver.common.keys import Keys
 s = Service(executable_path='chromedriver_win32 (3)\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 SEQUENCE = 'CC'
-TAG = 'Постоянные егены'
-URL = 'https://ruscorpora.ru/results?search=CnkSWQpXChQKC2xhbmdfc2VhcmNoEgUKA3J1cxI/CgkKA2xleBICCgAKCgoEZm9ybRICCgAKDgoFZ3JhbW0SBQoDKEEpCgkKA3NlbRICCgAKCwoFZmxhZ3MSAgoAKhAKCAgAEAoYMiAyIABABXgBMgcIBRIDZW5nOgEBMAE='
+TAG = 'Постоянные собственные тайгены'
+URL = 'https://ruscorpora.ru/results?search=CowBEmwKagoUCgtsYW5nX3NlYXJjaBIFCgNydXMSUgoJCgNsZXgSAgoACgoKBGZvcm0SAgoACiEKBWdyYW1tEhgKFihmYW1uIHwgcGVyc24gfCBwYXRybikKCQoDc2VtEgIKAAoLCgVmbGFncxICCgAqEAoICAAQChgyIDIgAEAFeAEyBwgFEgNlbmc6AQIwAQ=='
 try:
     driver.maximize_window()
     driver.get(URL)
@@ -20,7 +20,7 @@ try:
     time.sleep(2)
     for i in range(117):
         driver.find_element(By.CSS_SELECTOR, '#rnc-spa > div.results-page > div:nth-child(1) > div:nth-child(2) > div > div > div > div > div.col-12.col-md-8.col-lg-7 > div > ul > li.ant-pagination-next > button').click()
-        time.sleep(20)
+        time.sleep(50)
         savePars(driver.page_source, TAG)
         time.sleep(4)
    
